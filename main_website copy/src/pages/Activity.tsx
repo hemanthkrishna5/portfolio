9d31f66e4785fb3fd62a66f619efb6465e92d282
hemanthA <hemanth.ambarukhana@gmail.com>
2025-10-09 10:30:56 +0200

    removed duplicates

--- main_website copy/src/pages/Activity.tsx
+++
@@ -1,8 +0,0 @@
-export function Activity() {
-  return (
-    <div className="container" style={{ paddingTop: '48px', paddingBottom: '48px' }}>
-      <h1>Activity tracker</h1>
-      <p><a href="https://activity.tesseract.sbs" target="_blank" rel="noopener">Open dashboard</a></p>
-    </div>
-  );
-}
